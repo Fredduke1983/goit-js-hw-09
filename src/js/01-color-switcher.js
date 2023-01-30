87c8af85ff6672b3c426a74a3c67dcf2a217c39c
Nechai_Ihor <fredduke1983@gmail.com>
2023-01-30 23:10:27 +0200

    up

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
 
+let timerId;
+
+function randomBackground() {
+  body.style.backgroundColor = `${getRandomHexColor()}`;
+}
+
+function startIntervalTimer() {
+  timerId = setInterval(randomBackground, 1000);
+  btnStart.setAttribute('disabled', '');
+  btnStop.removeAttribute('disabled');
+}
+
+function stopIntervalTimer() {
+  clearInterval(timerId);
+  btnStop.setAttribute('disabled', '');
+  btnStart.removeAttribute('disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+btnStart.addEventListener('click', startIntervalTimer);
+btnStop.addEventListener('click', stopIntervalTimer);
