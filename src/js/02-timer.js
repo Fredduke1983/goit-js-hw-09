3407d5bd59a229636d3541a6c1c93dcd7cc8229e
Nechai_Ihor <fredduke1983@gmail.com>
2023-01-31 23:03:16 +0200

    up

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,6 +2,9 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
 const startBtn = document.querySelector('[data-start]');
+let chosenDate;
+let leftDays;
+
 startBtn.disabled = true;
 
 const options = {
@@ -10,7 +13,20 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    chosenDate = Number(selectedDates[0]);
+    console.log(this.defaultDate);
+    leftDays = chosenDate - this.defaultDate;
+    leftDays < 0 ? (startBtn.disabled = true) : (startBtn.disabled = false);
   },
 };
+
 flatpickr('#datetime-picker', options);
+
+setInterval(foo, 1000);
+
+function foo() {
+  //   const currentDate = new Date();
+  //   console.log(options.defaultDate);
+  options.defaultDate;
+}
+startBtn.addEventListener('click', () => console.log(leftDays));
