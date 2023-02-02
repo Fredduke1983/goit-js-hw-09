7b760e61c3e99692750c1fc77b9b158730a0a2f7
Nechai_Ihor <fredduke1983@gmail.com>
2023-02-02 23:35:08 +0200

    start task-03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,19 @@
+const delay = document.querySelector('[name=delay]');
+const step = document.querySelector('[name=step]');
+const amount = document.querySelector('[name=amount]');
+const submitBtn = document.querySelector('[type=submit]');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((res, rej) => {
+    if (shouldResolve) {
+      console.log('res');
+      // Fulfill
+    } else {
+      console.log('rej');
+      // Reject
+    }
+  });
 }
+
+submitBtn.addEventListener('click', createPromise(1, 2));
