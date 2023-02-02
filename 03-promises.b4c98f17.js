02d910bb1dfe667638b66f847b0ac5ae797f000f
Fredduke1983 <fredduke1983@gmail.com>
2023-02-02 21:48:47 +0000

    Deploying to gh-pages from @ Fredduke1983/goit-js-hw-09@10e672160d66613c11ce674dd816ee875a9cb49e 🚀

--- 03-promises.b4c98f17.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("[name=delay]"),document.querySelector("[name=step]"),document.querySelector("[name=amount]");document.querySelector("[type=submit]").addEventListener("click",function(e,o){const n=Math.random()>.3;new Promise(((e,o)=>{n?console.log("res"):console.log("rej")}))}());
-//# sourceMappingURL=03-promises.b4c98f17.js.map
