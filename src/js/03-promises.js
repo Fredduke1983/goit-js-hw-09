fb954f19802121b075d4569988c7bfad869d8242
Nechai_Ihor <fredduke1983@gmail.com>
2023-02-03 11:35:37 +0200

    up

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -8,7 +8,7 @@ Notiflix.Notify.init({
   opacity: 1,
 });
 
-const delay = document.querySelector('[name=delay]');
+let delay = document.querySelector('[name=delay]');
 const step = document.querySelector('[name=step]');
 const amount = document.querySelector('[name=amount]');
 const submitBtn = document.querySelector('[type=submit]');
@@ -19,16 +19,26 @@ function createPromise(position, delay) {
     setTimeout(() => {
       if (shouldResolve) {
         Notiflix.Notify.success('res');
+        console.log('delay in promise  ===', delay);
         // Fulfill
       } else {
-        Notiflix.Notify.success('rej');
+        Notiflix.Notify.failure('rej');
         // Reject
       }
-    }, 2000);
+    }, Number(delay));
   });
 }
 
-submitBtn.addEventListener('click', e => {
+submitBtn.addEventListener('click', onSubmit);
+let stepDelay = delay.value;
+
+function onSubmit(e) {
   e.preventDefault();
-  for (let i = 0; i < amount.value; i++) createPromise(i, delay.value);
-});
+  console.log('delay in sub ===', delay.value);
+  console.log('step in sub ===', step.value);
+  for (let i = 0; i < amount.value; i++) {
+    stepDelay = Number(stepDelay) + Number(step.value);
+    console.log('stepDelay ===', stepDelay);
+    createPromise(i, stepDelay);
+  }
+}
